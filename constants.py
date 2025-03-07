ce1ce81811c20347be5579478e60c4069a7e0c7c
Praveen952k4 <149948442+Praveen952k4@users.noreply.github.com>
2025-03-07 23:13:21 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,6 @@
+MONGODB_URI = "mongodb+srv://pradeepeng2535:luXt72JOHhh19Lne@cluster0.67vdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+GOOGLE_API_KEY = ''
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+
+REFRESH_TOKEN = ''
