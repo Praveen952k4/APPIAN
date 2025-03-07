0fb112111234d13c12207eb593da8a27d28958f8
Praveen952k4 <149948442+Praveen952k4@users.noreply.github.com>
2025-03-07 23:10:10 +0530

    Delete constants.py

--- constants.py
+++
@@ -1,7 +0,0 @@
-MONGODB_URI = "mongodb+srv://pradeepeng2535:luXt72JOHhh19Lne@cluster0.67vdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-GOOGLE_API_KEY = 'AIzaSyDY6faZaxhHoc1bobAwOogirwDGbeKkJFA'
-CLIENT_ID = '589520121396-3mbemgfsired90k6e4p3cl7rntv1edit.apps.googleusercontent.com'
-CLIENT_SECRET = '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'
-
-REFRESH_TOKEN = '1//04Xd1oW0y6AZxCgYIARAAGAQSNwF-L9IrCDRnI-Vk85W4tOmiZE7hsW3E3BmUyzy-lCz8sKcUtu2k0ZyzgmZxT2yipAP7PzzEyYc'
-
